9143775f56b4bf0a00a463e12a1adc8b12ca2240
Kiitaan <christian.lim@outlook.fr>
2018-07-05 15:31:59 +0200

    Updatinf comment state in our store

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Comments extends Component {
+const Comments = React.createClass({
 
 	renderComment(comment, i) {
 		return (
-			<div className='comments' key={i}>
+			<div className="comments" key={i}>
 				<p>
 					<strong>{comment.user}</strong>
 					{comment.text}
-					<button className='remove-comment'>&times; </button>
+					<button className="remove-comment">&times;</button>
 				</p>
 			</div>
-		)
-	};
+			)
+	},
+	handleSubmit(e) {
+		e.preventDefault();
+		const {postId} = this.props.params;
+		const author = this.refs.author.value;
+		const comment = this.refs.comment.value;
+		this.props.addComment(postId, author, comment);
+	},
 
 	render() {
+
 		return (
-			<div className='comments'>
+			<div className="comments">
 				{this.props.postComments.map(this.renderComment)}
-				<form ref="commentForm" className="comment-form">
-					<input type='text' ref='author' placeholder='author'/>
-					<input type='text' ref='comment' placeholder='comment'/>
-					<input type='submit' hidden />
+				<form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+					<input type="text" ref="author" placeholder="author"/>
+					<input type="text" ref="comment" placeholder="comment"/>
+					<input type="submit" hidden />
 				</form>
 			</div>
 		)
 	}
-}
+});
 
 export default Comments;
